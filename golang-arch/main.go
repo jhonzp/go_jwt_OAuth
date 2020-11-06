47534637203d476e77a2d2c759d349c4ea539832
Jhon Zuñiga <zuni230669>
2020-11-06 10:54:41 -0500

    Encode Example

--- golang-arch/main.go
+++ golang-arch/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
+	"net/http"
 )
 
 type person struct {
@@ -11,30 +11,48 @@ type person struct {
 }
 
 func main() {
-	p1 := person{
-		First: "Jhon",
-	}
+	// p1 := person{
+	// 	First: "Jhon",
+	// }
 
-	p2 := person{
-		First: "Rocio",
-	}
+	// p2 := person{
+	// 	First: "Rocio",
+	// }
 
-	xp := []person{p1, p2}
-	bs, err := json.Marshal(xp)
+	// xp := []person{p1, p2}
+	// bs, err := json.Marshal(xp)
+
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+
+	// fmt.Println("Print Json:", string(bs))
+
+	// xp2 := []person{}
+	// err = json.Unmarshal(bs, &xp2)
+
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+
+	// fmt.Println("back into go data Structure", xp2)
+
+	http.HandleFunc("/encode", foo)
+	http.HandleFunc("/decode", bar)
+	http.ListenAndServe(":8082", nil)
+
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	p := person{First: "Jhon Encode"}
+	err := json.NewEncoder(w).Encode(p)
 
 	if err != nil {
-		log.Panic(err)
+		log.Println("Encoded bad data!", err)
 	}
 
-	fmt.Println("Print Json:", string(bs))
+}
 
-	xp2 := []person{}
-	err = json.Unmarshal(bs, &xp2)
-
-	if err != nil {
-		log.Panic(err)
-	}
-
-	fmt.Println("back into go data Structure", xp2)
+func bar(w http.ResponseWriter, r *http.Request) {
 
 }
